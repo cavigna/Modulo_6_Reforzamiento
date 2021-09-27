23da80a3af69b0c3a2a5a3bfd5b902bda462ef64
cavigna <40524472+cavigna@users.noreply.github.com>
2021-09-26 21:11:38 -0300

    Added Ejer 6-8

--- 01_Ejercicios_Complementarios/POO/src/com/nacho/Main.java
+++ 01_Ejercicios_Complementarios/POO/src/com/nacho/Main.java
@@ -1,8 +1,23 @@
 package com.nacho;
 
+import com.nacho.bicicleta.Bicicleta;
+import com.nacho.bicicleta.BicicletaMontana;
+import com.nacho.bicicleta.BicicletaTandem;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // Ejercicio 7
+
+        Bicicleta[] bicicletas = {
+                new BicicletaTandem(1, 1, 1, 1),
+                new BicicletaTandem(1, 1, 1, 1),
+                new BicicletaMontana(1, 1, 1, 1),
+                new BicicletaMontana(1, 1, 1, 1),
+        };
+
+        for (int i = 0 ;  i<bicicletas.length; i++){
+            bicicletas[i].acelerar();
+        }
     }
 }
---
+++ 01_Ejercicios_Complementarios/POO/src/com/nacho/vehiculo/Vehiculo.java
@@ -0,0 +1,13 @@
+package com.nacho.vehiculo;
+
+
+/*
+EJERCICIO 8
+ */
+public abstract class Vehiculo {
+
+    public void acelerar(){}
+    public void frenar(){}
+
+
+}
