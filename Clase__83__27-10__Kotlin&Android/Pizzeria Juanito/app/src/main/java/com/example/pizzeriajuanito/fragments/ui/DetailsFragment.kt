0e974986571081e34384afb1596ccf9fb8663adc
cavigna <40524472+cavigna@users.noreply.github.com>
2021-10-27 14:59:48 -0300

    Clase 83

--- Clase__83__27-10__Kotlin&Android/Pizzeria Juanito/app/src/main/java/com/example/pizzeriajuanito/fragments/ui/DetailsFragment.kt
+++ Clase__83__27-10__Kotlin&Android/Pizzeria Juanito/app/src/main/java/com/example/pizzeriajuanito/fragments/ui/DetailsFragment.kt
@@ -5,15 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.activityViewModels
 import com.example.pizzeriajuanito.R
 import com.example.pizzeriajuanito.databinding.FragmentDetailsBinding
+import com.example.pizzeriajuanito.model.Pizza
 import com.example.pizzeriajuanito.viewmodel.PizzaViewModel
 
 
 class DetailsFragment : Fragment() {
     private lateinit var binding: FragmentDetailsBinding
-    val viewModel: PizzaViewModel by activityViewModels()
+    private val viewModel: PizzaViewModel by activityViewModels()
+
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,16 +26,40 @@ class DetailsFragment : Fragment() {
     ): View? {
         binding = FragmentDetailsBinding.inflate(layoutInflater, container, false)
 
-        var total = viewModel.pizza.total
-        var cliente = viewModel.pizza.cliente
+        var pizza = viewModel.pizza
 
-        binding.textView.text = total.toString()
-        binding.textView2.text = cliente.toString()
+        with(binding){
+            tvNombre.text = "Felicitaciones ${pizza.cliente}, tu pizza va en camino!"
+            tvdetalles.text = imprimir(pizza)
+            tvtotal.text = "Total = ${pizza.total}"
+        }
+
+
+
 
 
 
         return binding.root
 
+    }
+
+    fun imprimir(pizza:Pizza) :String{
+
+        var resultado = """Tamaño : ${pizza.size}
+            |
+            |
+        """.trimMargin()
+
+        if (pizza.carne != 0) resultado    +=  "Carne:     ${pizza.carne}\n\n"
+        if (pizza.pepe != 0) resultado     +=  "Peperoni:  ${pizza.pepe}\n\n"
+        if (pizza.tocino != 0) resultado   +=  "Tocino:    ${pizza.tocino}\n\n"
+        if (pizza.champ != 0) resultado    +=  "Champiñon: ${pizza.tocino}\n\n"
+        if (pizza.tomate != 0) resultado   +=  "Tomate:    ${pizza.tomate}\n\n"
+        if (pizza.choclo != 0) resultado   +=  "Choclo:    ${pizza.choclo}\n\n"
+        if (pizza.aceituna != 0) resultado +=  "Aceituna:  ${pizza.aceituna}\n\n"
+
+
+        return resultado
     }
 
 
--- Clase__83__27-10__Kotlin&Android/Pizzeria Juanito/app/src/main/java/com/example/pizzeriajuanito/fragments/ui/HomeFragment.kt
+++ Clase__83__27-10__Kotlin&Android/Pizzeria Juanito/app/src/main/java/com/example/pizzeriajuanito/fragments/ui/HomeFragment.kt
@@ -1,14 +1,17 @@
 package com.example.pizzeriajuanito.fragments.ui
 
+import android.app.Activity
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.RadioButton
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.pizzeriajuanito.R
@@ -46,11 +49,6 @@ class HomeFragment : Fragment() {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
 
         hacerBinding()
-//
-//        val editText = textInputLayout.editText as TextInputEditText
-//        editText.text
-
-
 
 
         boton.setOnClickListener{
@@ -59,7 +57,7 @@ class HomeFragment : Fragment() {
             Log.i("pizzeria", pizza.toString() )
 
             viewModel.pizza = pizza
-            //viewModel.pizza.cliente = txt
+
             findNavController().navigate(R.id.detailsFragment)
         }
 
